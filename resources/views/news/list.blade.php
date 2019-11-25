91f453a150c714a8fbf8eaa8551e5f0dd0913de2
lixiuye <hai30@163.com>
2019-11-25 17:10:29 +0800

    update content style

--- resources/views/news/list.blade.php
+++ resources/views/news/list.blade.php
@@ -16,66 +16,64 @@
 @section('content')
     @include('news.banner')
 
-    <div class="blank-block"></div>
-
-    <div class="container">
-        <div class="row">
-            <div class="col-12 col-lg-8">
-                <div class="row">
-                    <div class="col-12 col-lg-4">
-                        <img class="banner" src="{{ 'http://' .env('CDN_DOMAIN').'/'. $promotion->cover }}" alt=""/>
-                    </div>
-                    @if(Agent::isPhone())
-                    <div class="blank-block-m"></div>
-                    @endif
-                    <div class="col-12 col-lg-8">
-                        <a href="{{ route('news.show', ['id' => $promotion->id]) }}">
-                            <h5>{{ $promotion->title }}</h5>
-                            <p class="small-font">{{ \Carbon\Carbon::createFromTimeStamp(strtotime($promotion->updated_at))->format('Y-m-d') }}</p>
-                            <hr>
-                            <p>
-                                {{ $promotion->description }}
-                            </p>
-                        </a>
+    <div class="container-fluid grid-back">
+        <div class="blank-block"></div>
+        <div class="container">
+            <div class="row">
+                <div class="col-12 col-lg-8">
+                    <div class="artical-container">
+                        <div class="row">
+                            <div class="col-12 col-lg-4">
+                                <img class="banner" src="{{ 'http://' .env('CDN_DOMAIN').'/'. $promotion->cover }}" alt=""/>
+                            </div>
+                            @if(Agent::isPhone())
+                                <div class="blank-block-m"></div>
+                            @endif
+                            <div class="col-12 col-lg-8">
+                                <a href="{{ route('news.show', ['id' => $promotion->id]) }}">
+                                    <h5>{{ $promotion->title }}</h5>
+                                    <p class="small-font">{{ \Carbon\Carbon::createFromTimeStamp(strtotime($promotion->updated_at))->format('Y-m-d') }}</p>
+                                    <hr>
+                                    <p>
+                                        {{ $promotion->description }}
+                                    </p>
+                                </a>
+                            </div>
+                        </div>
+                        <div class="blank-block"></div>
+                        <ul style="padding: 0">
+                            @foreach($news as $item)
+                                <a href="{{ route('news.show', ['id' => $item->id]) }}">
+                                    <li>
+                                        <div class="row justify-content-between">
+                                            <div class="col-auto mr-auto">
+                                                {{ $item->title }}
+                                            </div>
+                                            <div class="col-auto">
+                                                {{ \Carbon\Carbon::createFromTimeStamp(strtotime($item->updated_at))->format('Y-m-d') }}
+                                            </div>
+                                        </div>
+                                        <hr>
+                                    </li>
+                                </a>
+                            @endforeach
+                        </ul>
                     </div>
                 </div>
-                <div class="blank-block"></div>
-                <ul style="padding: 0">
-                    @foreach($news as $item)
-                        <a href="{{ route('news.show', ['id' => $item->id]) }}">
-                            <li>
-                                <div class="row justify-content-between">
-                                    <div class="col-auto mr-auto">
-                                        {{ $item->title }}
-                                    </div>
-                                    <div class="col-auto">
-                                        {{ \Carbon\Carbon::createFromTimeStamp(strtotime($item->updated_at))->format('Y-m-d') }}
-                                    </div>
-                                </div>
-                                <hr>
-                            </li>
-                        </a>
-                    @endforeach
-                </ul>
-            </div>
-            @if(Agent::isPhone())
-                <div class="blank-block-m"></div>
-            @endif
-            <div class="col-12 col-lg-4">
-                <div class="text-center" style="background-color: #eee; margin-left: 16px; padding: 36px;">
-                    <h5 style="color: #666">信息检索</h5>
-                    <br>
-                    <input class="form-control" type="text" placeholder="搜索">
-                    <br>
-                    <button type="submit" class="btn btn-primary mb-2 btn-block">搜索</button>
+                @if(Agent::isPhone())
+                    <div class="blank-block-m"></div>
+                @endif
+                <div class="col-12 col-lg-4">
+                    <div class="text-center artical-container sticky-top">
+                        <h5 style="color: #666">信息检索</h5>
+                        <br>
+                        <input class="form-control" type="text" placeholder="搜索">
+                        <br>
+                        <button type="submit" class="btn btn-primary mb-2 btn-block">搜索</button>
+                    </div>
                 </div>
             </div>
         </div>
+        <div class="blank-block"></div>
     </div>
-
-
-    <div class="blank-block"></div>
-
-
-
 @stop
--- resources/views/news/show.blade.php
+++ resources/views/news/show.blade.php
@@ -28,36 +28,32 @@
 @section('content')
    @include('news.banner')
 
-    <div class="blank-block"></div>
-    <div class="blank-block"></div>
+    <div class="container-fluid grid-back">
+        <div class="blank-block"></div>
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-8">
+                    <div class="artical-container">
+                         <div class="text-center">
+                             <h2>{{ $news->title ?? '标题'}}</h2>
+                             <br>
+                             <p class="small-font">发布时间: {{ \Carbon\Carbon::createFromTimeStamp(strtotime($news->updated_at))->format('Y-m-d') }}</p>
+                         </div>
 
-    <div class="container">
-        <div class="row">
-            <div class="col-lg-8">
-                <div class="artical-container">
-                    <div class="row">
-                        <div class="col-12">
-                            <div class="text-center">
-                                <h2>{{ $news->title ?? '标题'}}</h2>
-                                <br>
-                                <p class="small-font">发布时间: {{ \Carbon\Carbon::createFromTimeStamp(strtotime($news->updated_at))->format('Y-m-d') }}</p>
-                            </div>
-
-                            <div class="blank-block"></div>
-                            <div class="font-normal" style="text-indent: 0">
-                                {!! $news->content !!}
-                            </div>
-                        </div>
+                         <div class="blank-block"></div>
+                         <div class="font-normal" style="text-indent: 0">
+                                    {!! $news->content !!}
+                                </div>
                     </div>
                 </div>
-            </div>
-            <div class="col-lg-4">
-                <div class="text-center" style="background-color: #eee; margin-left: 16px; padding: 36px;">
-                    <h5 style="color: #666">信息检索</h5>
-                    <br>
-                    <input class="form-control" type="text" placeholder="搜索">
-                    <br>
-                    <button type="submit" class="btn btn-primary mb-2 btn-block">搜索</button>
+                <div class="col-lg-4">
+                    <div class="text-center sticky-top artical-container" >
+                        <h5 style="color: #666">信息检索</h5>
+                        <br>
+                        <input class="form-control" type="text" placeholder="搜索">
+                        <br>
+                        <button type="submit" class="btn btn-primary mb-2 btn-block">搜索</button>
+                    </div>
                 </div>
             </div>
         </div>
